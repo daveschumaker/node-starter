ab3ea0c08c2ad308413968bdf5e15401cbe0b21d
Dave Schumaker <daves@hotpads.com>
2020-03-03 11:56:43 -0800

    Update build process and support aliases and async generators

--- bin/main.js
+++ bin/main.js
@@ -1,1 +1,7 @@
+// core-js and regenerator-runtime should be the first line in
+// entry point of app, so that async generators are useable
+// when babel compiles and builds the app.
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 console.log('HELLO WORLD!');
